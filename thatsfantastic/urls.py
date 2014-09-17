c2c11ead41f3ee1ab8056a10143ff77530c12581
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-09-17 14:37:37 -0400

    Update styles, settings for delivering static assets when debugging.

--- thatsfantastic/urls.py
+++ thatsfantastic/urls.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from thatsfantastic.views import (FilmDetailView, FilmSearchView, FilmListView)
 
+DEBUG = getattr(settings, 'DEBUG', False)
 
 urlpatterns = patterns('',
     url(r'^films/(?P<pk>\d+)/$', FilmDetailView.as_view(), name='film-detail'),
@@ -10,3 +12,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
