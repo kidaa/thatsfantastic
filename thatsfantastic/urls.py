6b24d10a151171ea64d4d334785ebd821ded7bc2
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-09-17 16:39:36 -0400

    Fix search url, button in template. Style tweaks.

--- thatsfantastic/urls.py
+++ thatsfantastic/urls.py
@@ -6,8 +6,8 @@ from thatsfantastic.views import (FilmDetailView, FilmSearchView, FilmListView)
 DEBUG = getattr(settings, 'DEBUG', False)
 
 urlpatterns = patterns('',
+    url(r'^search/films/$', FilmSearchView.as_view(), name='films-search'),
     url(r'^films/(?P<slug>[\w\-]+)/$', FilmDetailView.as_view(), name='film-detail'),
-    url(r'^films/search/$', FilmSearchView.as_view(), name='films-search'),
     url(r'^$', FilmListView.as_view(), name='films-list'),
 
     url(r'^admin/', include(admin.site.urls)),
